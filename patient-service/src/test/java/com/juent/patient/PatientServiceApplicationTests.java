22cf41eff659203cbdfd5e4852825eaa97ab4ebf
Julien <julient.bourgeois@gmail.com>
2025-03-06 11:24:38 +0100

    first commit

---
+++ patient-service/src/test/java/com/juent/patient/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juent.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
